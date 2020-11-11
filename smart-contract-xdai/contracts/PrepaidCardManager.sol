b8b292e46ea285b5a24b180c0cc35b8281410711
Nguyễn Minh Tâm <ngmitamit@gmail.com>
2020-11-11 17:41:46 +0700

    Add PrepaidCardManager contract to manager create and split card; Add splitCard method CardModule

---
+++ smart-contract-xdai/contracts/PrepaidCardManager.sol
@@ -0,0 +1,202 @@
+pragma solidity >=0.5.17;
+
+import "@gnosis.pm/safe-contracts/contracts/proxies/GnosisSafeProxyFactory.sol";
+import "@gnosis.pm/safe-contracts/contracts/libraries/CreateAndAddModules.sol";
+import "@openzeppelin/contracts/ownership/Ownable.sol";
+import "@openzeppelin/contracts/math/SafeMath.sol";
+
+//BytesLib https://github.com/GNSPS/solidity-bytes-utils
+import "solidity-bytes-utils/contracts/BytesLib.sol";
+
+//modules
+import "./modules/CardModule.sol";
+
+import "./token/IERC677.sol";
+import "./roles/Tally.sol";
+import "./roles/PayableToken.sol";
+
+contract PrepaidCardManage is Tally, PayableToken {
+    using BytesLib for bytes;
+    using SafeMath for uint256;
+
+    event CreatePrepaidCard(
+        address supplier,
+        address card,
+        address token,
+        uint256 amount
+    );
+    event SplitCard(
+        address supplier,
+        address card,
+        address token,
+        uint256 number,
+        uint256 amount,
+        address[] list
+    );
+
+    using SafeMath for uint256;
+
+    address private gsMasterCopy;
+    address private gsProxyFactory;
+    address private gsCreateAndAddModules;
+    address private revenuePool;
+    address private cardModule;
+
+    function setup(
+        address _tally,
+        address[] memory _gnosisSafe,
+        address[] memory _payableTokens
+    ) public onlyOwner {
+        // setup tally user
+        addTally(_tally);
+        /**
+        setup gnosis safe address
+        _gnosisSafe[0] is masterCopy address
+        _gnosisSafe[1] is gnosis proxy factory address
+        _gnosisSafe[2] is gnosis CreateAndAddModules 
+        _gnosisSafe[3] is revenuePool address
+        _gnosisSafe[3] is cardModule address
+        */
+        gsMasterCopy = _gnosisSafe[0];
+        gsProxyFactory = _gnosisSafe[1];
+        gsCreateAndAddModules = _gnosisSafe[2];
+        revenuePool = _gnosisSafe[3];
+        cardModule = _gnosisSafe[4];
+
+        // set token list payable.
+        for (uint256 i = 0; i < _payableTokens.length; ++i) {
+            addPayableToken(_payableTokens[i]);
+        }
+    }
+
+    /**
+     * @dev Create Prepaid card
+     * @param supplier - Supplier address
+     * @param token - Token address
+     * @param amount - Amount of Prepaid card
+     * @return PrepaidCard address
+     */
+    function _createPrepaidCard(
+        address supplier,
+        address token,
+        uint256 amount
+    ) private returns (address) {
+        address[] memory owners = new address[](2);
+        owners[0] = owner();
+        owners[1] = supplier;
+
+        bytes memory moduleDataPayloads = abi.encodeWithSignature(
+            "setup(address,address,address)",
+            revenuePool,
+            this,
+            owners[0]
+        );
+        bytes memory proxyFactoryDataPayloads = abi.encodeWithSignature(
+            "createProxy(address,bytes)",
+            cardModule,
+            moduleDataPayloads
+        );
+        bytes memory modulesCreationDataPayloads = proxyFactoryDataPayloads
+            .slice(72, proxyFactoryDataPayloads.length.add(72));
+        bytes memory createAndAddModulesDataPayloads = abi.encodeWithSignature(
+            "createAndAddModules(address,bytes)",
+            gsProxyFactory,
+            modulesCreationDataPayloads
+        );
+        bytes memory payloads = abi.encodeWithSignature(
+            "setup(address[],uint256,address,bytes,address,address,uint256,address)",
+            owners,
+            2,
+            address(0),
+            createAndAddModulesDataPayloads,
+            address(0),
+            address(0),
+            0,
+            address(0)
+        );
+
+        address card = address(
+            GnosisSafeProxyFactory(gsProxyFactory).createProxy(
+                gsMasterCopy,
+                payloads
+            )
+        );
+
+        require(card != address(0), "Could not create card");
+
+        require(
+            IERC677(token).transfer(card, amount),
+            "Could not transfer token to card"
+        );
+
+        emit CreatePrepaidCard(supplier, card, token, amount);
+
+        return card;
+    }
+
+    /**
+     * @dev Split Prepaid card
+     * @param supplier - Supplier address
+     * @param card - Prepaid card address
+     * @param token - Token address
+     * @param amount - Amount to split
+     * @param payloads - Payloads
+     */
+    function _split(
+        address supplier,
+        address card,
+        address token,
+        uint256 amount,
+        bytes memory payloads
+    ) private returns (address[] memory) {
+        uint256 number;
+        uint256 amountPerCard;
+
+        (number, amountPerCard) = abi.decode(payloads, (uint256, uint256));
+
+        require(number > 0, "number of card must be greater than 0");
+        require(amountPerCard > 0, "amount per card must be greater than 0");
+        require(
+            amount >= number.mul(amountPerCard),
+            "your amount must be >= (number of card) * (amount per card)"
+        );
+
+        address[] memory list = new address[](number);
+        address newCard;
+        for (uint256 p = 0; p < number; p++) {
+            newCard = _createPrepaidCard(supplier, token, amountPerCard);
+            require(newCard != address(0));
+            list[p] = newCard;
+        }
+
+        emit SplitCard(supplier, card, token, number, amountPerCard, list);
+
+        return list;
+    }
+
+    /**
+     * @dev tokenFallback(ERC677) - call when token receive pool.
+     * we will exchange receive token to SPEND token and transfer it to wallet of merchant.
+     * @param from - Supplier or Prepaid card address
+     * @param amount - number token them pay.
+     * @param data - data encoded
+     */
+    function tokenFallback(
+        address from,
+        uint256 amount,
+        bytes calldata data
+    ) external onlyPayableToken() returns (bool) {
+        address supplier;
+        bytes memory payloads;
+        (supplier, payloads) = abi.decode(data, (address, bytes));
+
+        if (supplier != address(0)) {
+            _split(supplier, from, _msgSender(), amount, payloads);
+        } else {
+            require(
+                _createPrepaidCard(from, _msgSender(), amount) != address(0)
+            );
+        }
+        return true;
+    }
+}
--- smart-contract-xdai/contracts/modules/CardModule.sol
+++ smart-contract-xdai/contracts/modules/CardModule.sol
@@ -1,70 +1,144 @@
-pragma solidity >=0.5.0 <0.7.0;
-import '@gnosis.pm/safe-contracts/contracts/base/Module.sol';
-import '@gnosis.pm/safe-contracts/contracts/base/ModuleManager.sol';
-import '@gnosis.pm/safe-contracts/contracts/base/OwnerManager.sol';
-import '@gnosis.pm/safe-contracts/contracts/common/Enum.sol';
+pragma solidity >=0.5.17;
+import "@gnosis.pm/safe-contracts/contracts/base/Module.sol";
+import "@gnosis.pm/safe-contracts/contracts/base/ModuleManager.sol";
+import "@gnosis.pm/safe-contracts/contracts/base/OwnerManager.sol";
+import "@gnosis.pm/safe-contracts/contracts/common/Enum.sol";
+import "@openzeppelin/contracts/math/SafeMath.sol";
 
 contract CardModule is Module {
-  string public constant NAME = 'Card Module';
-  string public constant VERSION = '0.1.0';
-  address REVENUE_POOL_ADDRESS;
-  address CARD_STACK_ADMIN_ADDRESS;
-  bool sold;
+    using SafeMath for uint256;
+    string public constant NAME = "Card Module";
+    string public constant VERSION = "0.1.0";
+    address REVENUE_POOL_ADDRESS;
+    address CARD_STACK_ADMIN_ADDRESS;
+    address PREPAID_CARD_MANAGER_ADDRESS;
+    bool sold;
 
-  /// @dev Setup function sets initial storage of contract.
-  /// @param revenuePoolAddress Revenue Pool Address
-  /// @param admin Admin address
-  function setup(address revenuePoolAddress, address admin) public {
-    setManager();
-    REVENUE_POOL_ADDRESS = revenuePoolAddress;
-    CARD_STACK_ADMIN_ADDRESS = admin;
-  }
+    /**
+     * @dev Setup function sets initial storage of contract.
+     * @param revenuePoolAddress Revenue Pool Address
+     * @param PrepaidCardManagerAddress Prepaid card manager Address
+     * @param admin Admin address
+     */
+    function setup(
+        address revenuePoolAddress,
+        address PrepaidCardManagerAddress,
+        address admin
+    ) public {
+        setManager();
+        REVENUE_POOL_ADDRESS = revenuePoolAddress;
+        CARD_STACK_ADMIN_ADDRESS = admin;
+        PREPAID_CARD_MANAGER_ADDRESS = PrepaidCardManagerAddress;
+    }
 
-  modifier onlyOwner() {
-    // Prevent Card stack admin use
-    require(msg.sender != CARD_STACK_ADMIN_ADDRESS, 'Card stack admin can not use');
-    // Only Safe owners are allowed to execute transactions.
-    require(OwnerManager(address(manager)).isOwner(msg.sender), 'Method can only be called by an owner');
-    _;
-  }
+    modifier onlyOwner() {
+        // Prevent Card stack admin use
+        require(
+            msg.sender != CARD_STACK_ADMIN_ADDRESS,
+            "Card stack admin can not use"
+        );
+        // Only Safe owners are allowed to execute transactions.
+        require(
+            OwnerManager(address(manager)).isOwner(msg.sender),
+            "Method can only be called by an owner"
+        );
+        _;
+    }
 
-  /// @dev Sell to customer
-  /// @param to Address of new owner
-  /// @return Returns if transaction can be executed.
-  function sellCard(address to) public onlyOwner {
-    // Only sell 1 time
-    require(!sold, 'The card has been sold before');
+    /**
+     * @dev split the card to multible card
+     * @param token Token address
+     * @param number Number of card want to split
+     * @param amountPerCard Amount per card
+     * @return Returns if transaction can be executed.
+     */
+    function splitCard(
+        address token,
+        uint256 number,
+        uint256 amountPerCard
+    ) public onlyOwner {
+        bytes memory data = abi.encodeWithSignature(
+            "transferAndCall(address,uint256,bytes)",
+            PREPAID_CARD_MANAGER_ADDRESS,
+            number.mul(amountPerCard),
+            abi.encodeWithSignature(
+                "address,bytes",
+                msg.sender,
+                abi.encodeWithSignature(
+                    "uint256,uint256",
+                    number,
+                    amountPerCard
+                )
+            )
+        );
+        require(
+            manager.execTransactionFromModule(
+                token,
+                0,
+                data,
+                Enum.Operation.Call
+            ),
+            "Could not split the card"
+        );
+    }
 
-    // Swap owner
-    // CARD_STACK_ADMIN_ADDRESS must be add before user address when crating card
-    bytes memory data = abi.encodeWithSignature(
-      'swapOwner(address,address,address)',
-      CARD_STACK_ADMIN_ADDRESS,
-      msg.sender,
-      to
-    );
-    require(manager.execTransactionFromModule(address(manager), 0, data, Enum.Operation.Call), 'Could not swap owner');
+    /**
+     * @dev Sell to customer
+     * @param to Address of new owner
+     * @return Returns if transaction can be executed.
+     */
+    function sellCard(address to) public onlyOwner {
+        // Only sell 1 time
+        require(!sold, "The card has been sold before");
 
-    // The card has been sold
-    sold = true;
-  }
+        // Swap owner
+        // CARD_STACK_ADMIN_ADDRESS must be add before user address when crating card
+        bytes memory data = abi.encodeWithSignature(
+            "swapOwner(address,address,address)",
+            CARD_STACK_ADMIN_ADDRESS,
+            msg.sender,
+            to
+        );
+        require(
+            manager.execTransactionFromModule(
+                address(manager),
+                0,
+                data,
+                Enum.Operation.Call
+            ),
+            "Could not swap owner"
+        );
 
-  /// @dev Pay to merchant
-  /// @param token Address of the token that should be used to pay
-  /// @param to Address of merchant
-  /// @param amount Amount of tokens that should be pay to merchant
-  /// @return Returns if transaction can be executed.
-  function pay(
-    address token,
-    address to,
-    uint256 amount
-  ) public onlyOwner {
-    bytes memory data = abi.encodeWithSignature(
-      'transferAndCall(address,uint256,bytes)',
-      REVENUE_POOL_ADDRESS,
-      amount,
-      abi.encode(to)
-    );
-    require(manager.execTransactionFromModule(token, 0, data, Enum.Operation.Call), 'Could not pay to merchant');
-  }
+        // The card has been sold
+        sold = true;
+    }
+
+    /**
+     * @dev Pay to merchant
+     * @param token Address of the token that should be used to pay
+     * @param to Address of merchant
+     * @param amount Amount of tokens that should be pay to merchant
+     * @return Returns if transaction can be executed.
+     */
+    function pay(
+        address token,
+        address to,
+        uint256 amount
+    ) public onlyOwner {
+        bytes memory data = abi.encodeWithSignature(
+            "transferAndCall(address,uint256,bytes)",
+            REVENUE_POOL_ADDRESS,
+            amount,
+            abi.encode(to)
+        );
+        require(
+            manager.execTransactionFromModule(
+                token,
+                0,
+                data,
+                Enum.Operation.Call
+            ),
+            "Could not pay to merchant"
+        );
+    }
 }
