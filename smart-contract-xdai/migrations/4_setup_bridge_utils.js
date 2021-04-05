b1119702e9bb884d8330e87b4a110821c79bfd76
Justin <justinthong93@gmail.com>
2021-04-05 19:56:39 +0800

    forgot to update bridge script with imports

--- smart-contract-xdai/migrations/4_setup_bridge_utils.js
+++ smart-contract-xdai/migrations/4_setup_bridge_utils.js
@@ -1,14 +1,11 @@
 const BridgeUtils = artifacts.require("BridgeUtils");
 const PrepaidCardManager = artifacts.require("PrepaidCardManager");
 const RevenuePool = artifacts.require("RevenuePool");
-
-const GNOSIS_SAFE_MASTER_COPY =
-  process.env.GNOSIS_SAFE_MASTER_COPY ??
-  `0x6851d6fdfafd08c0295c392436245e5bc78b0185`;
-const GNOSIS_SAFE_FACTORY =
-  process.env.GNOSIS_SAFE_FACTORY ??
-  `0x76E2cFc1F5Fa8F6a5b3fC4c8F4788F0116861F9B`;
-const BRIDGE_MEDIATOR = process.env.BRIDGE_MEDIATOR;
+const {
+  GNOSIS_SAFE_FACTORY,
+  GNOSIS_SAFE_MASTER_COPY,
+  BRIDGE_MEDIATOR,
+} = require("./constants");
 
 module.exports = async function (_, network) {
   if (network === "ganache") {
