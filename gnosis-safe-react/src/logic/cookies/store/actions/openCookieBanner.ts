3fd6c0c67b4f3b08b8e06a1bfe5d003f54c2f075
Hassan Abdel-Rahman <hassan.abdelrahman@gmail.com>
2021-04-05 16:43:00 -0400

    removing gnosis-safe-react

--- gnosis-safe-react/src/logic/cookies/store/actions/openCookieBanner.ts
+++
@@ -1,7 +0,0 @@
-import { createAction } from 'redux-actions'
-
-export const OPEN_COOKIE_BANNER = 'OPEN_COOKIE_BANNER'
-
-export const openCookieBanner = createAction(OPEN_COOKIE_BANNER, (cookieBannerOpen) => ({
-  cookieBannerOpen,
-}))
--- gnosis-safe-react/src/logic/safe/utils/buildSafeInformationUrl.ts
+++
@@ -1,8 +0,0 @@
-import { getSafeServiceBaseUrl } from 'src/config'
-import { checksumAddress } from 'src/utils/checksumAddress'
-
-export const buildSafeInformationUrl = (safeAddress: string): string => {
-  const address = checksumAddress(safeAddress)
-  const url = getSafeServiceBaseUrl(address)
-  return `${url}/`
-}
