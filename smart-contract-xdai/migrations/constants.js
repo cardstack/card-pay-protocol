7bd784879913184c366841adcd2bb2c5158f2cd3
Justin <justinthong93@gmail.com>
2021-04-05 19:46:37 +0800

    removed maximum and minimum in constants

--- smart-contract-xdai/migrations/constants.js
+++ smart-contract-xdai/migrations/constants.js
@@ -8,6 +8,4 @@ module.exports = {
   GNOSIS_SAFE_FACTORY:
     process.env.GNOSIS_SAFE_FACTORY ??
     `0x76E2cFc1F5Fa8F6a5b3fC4c8F4788F0116861F9B`,
-  MINIMUM_AMOUNT: process.env.MINIMUM_AMOUNT ?? 100, //minimum face value (in SPEND) for new prepaid card
-  MAXIMUM_AMOUNT: process.env.MAXIMUM_AMOUNT ?? 100000 * 100, //maximum face value (in SPEND) for new prepaid card
 };
