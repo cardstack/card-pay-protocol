a95c5ca411f99c1ac671e416085170ec3ae56eeb
Hassan Abdel-Rahman <hassan.abdelrahman@gmail.com>
2021-04-12 15:01:19 -0400

    adding gas measurement script for tests

---
+++ migrations/1_measure_deployment_gas.js
@@ -0,0 +1,17 @@
+const PrepaidCardManager = artifacts.require("PrepaidCardManager");
+const RevenuePool = artifacts.require("RevenuePool");
+const BridgeUtils = artifacts.require("BridgeUtils");
+const SPEND = artifacts.require("SPEND");
+
+// we only maintain these migrations purely to measure the amount of gas it
+// takes to perform a deployment for each contract
+module.exports = async function (deployer, network) {
+  if (network === "test") {
+    await Promise.all([
+      deployer.deploy(PrepaidCardManager),
+      deployer.deploy(RevenuePool),
+      deployer.deploy(BridgeUtils),
+      deployer.deploy(SPEND),
+    ]);
+  }
+};
