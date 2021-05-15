6dc3ee0baa6033a91778faedeb068bb5dab823f3
Hassan Abdel-Rahman <hassan.abdelrahman@gmail.com>
2021-05-14 22:43:15 -0400

    deployed v1.0.8

--- contracts/core/Versionable.sol
+++ contracts/core/Versionable.sol
@@ -2,7 +2,7 @@ pragma solidity 0.5.17;
 
 contract Versionable {
   function cardProtocolVersion() external pure returns (string memory) {
-    return "0.1.7";
+    return "0.1.8";
   }
 
   uint256[50] private ____gap;
