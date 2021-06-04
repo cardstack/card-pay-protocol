6266d7f76cd8298e739b381c38fefb21d091fe76
Hassan Abdel-Rahman <hassan.abdelrahman@gmail.com>
2021-06-04 10:25:28 -0400

    Revert "v0.4.0"
    This reverts commit 6f7f13a3b50fae09da5e0087974c83de18ea743f.

--- contracts/core/Versionable.sol
+++ contracts/core/Versionable.sol
@@ -2,7 +2,7 @@ pragma solidity 0.5.17;
 
 contract Versionable {
   function cardpayVersion() external pure returns (string memory) {
-    return "0.4.0";
+    return "0.3.4";
   }
 
   uint256[50] private ____gap;
