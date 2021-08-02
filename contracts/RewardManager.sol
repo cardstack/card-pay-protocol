f29a6f82811cf63f66a5ec1c55d94158447f1461
Justin <justinthong93@gmail.com>
2021-08-03 00:34:34 +0800

    fix new transfer reward manager

--- contracts/RewardManager.sol
+++ contracts/RewardManager.sol
@@ -4,6 +4,7 @@ import "@openzeppelin/contract-upgradeable/contracts/ownership/Ownable.sol";
 import "@openzeppelin/contract-upgradeable/contracts/utils/EnumerableSet.sol";
 import "@gnosis.pm/safe-contracts/contracts/GnosisSafe.sol";
 import "@gnosis.pm/safe-contracts/contracts/common/Enum.sol";
+import "@openzeppelin/contract-upgradeable/contracts/math/SafeMath.sol";
 
 import "./core/Safe.sol";
 import "./core/Versionable.sol";
@@ -12,6 +13,7 @@ import "./ActionDispatcher.sol";
 contract RewardManager is Ownable, Versionable, Safe {
     //Using
     using EnumerableSet for EnumerableSet.AddressSet;
+    using SafeMath for uint256;
 
     //Events
     event Setup();
@@ -160,7 +162,7 @@ contract RewardManager is Ownable, Versionable, Safe {
 
         owners[0] = address(this);
         owners[1] = prepaidCardOwner;
-        rewardSafe = createSafe(owners, 1);
+        rewardSafe = createSafe(owners, 2);
         rewardSafes[rewardProgramID][prepaidCardOwner] = rewardSafe;
         emit RewardeeRegistered(rewardProgramID, prepaidCardOwner);
         emit RewardSafeCreated(prepaidCardOwner, rewardSafe);
@@ -174,19 +176,43 @@ contract RewardManager is Ownable, Versionable, Safe {
         bytes calldata previousOwnerSignature,
         bytes calldata data
     ) external {
-        GnosisSafe(rewardSafe).execTransaction(
+        execTransaction(
             rewardSafe,
-            0,
+            rewardSafe,
             data,
-            Enum.Operation.Call,
-            0,
-            0,
-            0,
+            _addContractSignature(rewardSafe,previousOwnerSignature),
             gasToken,
-            gasRecipient,
-            previousOwnerSignature
+            gasRecipient
         );
         emit RewardSafeTransferred();
+    }
+
+    // somehow using a private function gets rid of the callstack too deep error
+    function execTransaction(
+        address payable rewardSafe,
+        address to,
+        bytes memory data,
+        bytes memory signatures,
+        address _gasToken,
+        address payable _gasRecipient
+    ) private returns (bool) {
+        require(
+            GnosisSafe(rewardSafe).execTransaction(
+                to,
+                0,
+                data,
+                Enum.Operation.Call,
+                0,
+                0,
+                0, //If there is no gas price, there will be no transfer to _gasRecipient
+                _gasToken,
+                _gasRecipient,
+                signatures
+            ),
+            "safe transaction was reverted"
+        );
+
+        return true;
     }
 
     function getTransferRewardSafeData(
@@ -296,5 +322,47 @@ contract RewardManager is Ownable, Versionable, Safe {
                 )
             ) ==
             keccak256(abi.encodePacked(rule2.tallyRuleDID, rule2.benefitDID));
+    }
+
+    function _getContractSignature()
+        internal
+        view
+        returns (bytes memory contractSignature)
+    {
+        // Create signature
+        contractSignature = new bytes(65);
+        bytes memory encodeData = abi.encode(this, address(0));
+        for (uint256 i = 1; i <= 64; i++) {
+            contractSignature[64 - i] = encodeData[encodeData.length.sub(i)];
+        }
+        bytes1 v = 0x01;
+        contractSignature[64] = v;
+    }
+
+    function _addContractSignature(
+        address payable rewardSafe,
+        bytes memory signature
+    ) internal view returns (bytes memory signatures) {
+        require(signature.length == 65, "Invalid signature!");
+
+        address owner = getRewardSafeOwner(rewardSafe);
+        bytes memory contractSignature = _getContractSignature();
+        signatures = new bytes(130); // 2 x 65 bytes
+        // Gnosis safe require signature must be sort by owner' address.
+        if (address(this) > owner) {
+            for (uint256 i = 0; i < signature.length; i++) {
+                signatures[i] = signature[i];
+            }
+            for (uint256 i = 0; i < contractSignature.length; i++) {
+                signatures[i.add(65)] = contractSignature[i];
+            }
+        } else {
+            for (uint256 i = 0; i < contractSignature.length; i++) {
+                signatures[i] = contractSignature[i];
+            }
+            for (uint256 i = 0; i < signature.length; i++) {
+                signatures[i.add(65)] = signature[i];
+            }
+        }
     }
 }
