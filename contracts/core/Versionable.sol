ffbc4b0b239e85bfbb84b5e6cc6d0b97ba664aa2
Hassan Abdel-Rahman <hassan.abdelrahman@gmail.com>
2021-08-09 13:46:37 -0400

    v0.7.0+sokol

--- contracts/core/Versionable.sol
+++ contracts/core/Versionable.sol
@@ -2,7 +2,7 @@ pragma solidity 0.5.17;
 
 contract Versionable {
   function cardpayVersion() external pure returns (string memory) {
-    return "0.6.7";
+    return "0.7.0";
   }
 
   uint256[50] private ____gap;
