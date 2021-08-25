3ab8f1d719087ad08f536135160d17262b1a6470
Alex Speller <alex@alexspeller.com>
2021-08-25 23:31:09 +0100

    Finish migrations and verifications

--- hardhat.config.js
+++ hardhat.config.js
@@ -4,20 +4,44 @@
 
 require("@nomiclabs/hardhat-truffle5");
 require("solidity-coverage");
-require('hardhat-contract-sizer');
-
+require("hardhat-contract-sizer");
+require("@openzeppelin/hardhat-upgrades");
+require("@nomiclabs/hardhat-ethers");
 
 module.exports = {
-  solidity: "0.5.17",
+  solidity: {
+    compilers: [
+      {
+        version: "0.5.17",
+        settings: {
+          evmVersion: "istanbul"
+        }
+      }
+    ]
+  },
+
   networks: {
     hardhat: {
       initialBaseFeePerGas: 0 // workaround from https://github.com/sc-forks/solidity-coverage/issues/652#issuecomment-896330136 . Remove when that issue is closed.
+    },
 
+    sokol: {
+      url: "https://sokol.poa.network",
+      chainId: 77,
+      gasPrice: 1000000000,
+      derivationPath: "m/44'/60'/0'/0/1"
+    },
+
+    xdai: {
+      url: "https://rpc.xdaichain.com/",
+      chainId: 100,
+      derivationPath: "m/44'/60'/0'/0/2",
+      gasPrice: 1000000000
     }
   },
   contractSizer: {
     alphaSort: true,
     runOnCompile: true,
-    disambiguatePaths: true,
+    disambiguatePaths: true
   }
 };
