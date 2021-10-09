9d1173550b8d5afab6bb9416fb3234c116a8031e
Justin <justinthong93@gmail.com>
2021-10-09 21:51:04 +0800

    add create2

--- contracts/core/Safe.sol
+++ contracts/core/Safe.sol
@@ -48,5 +48,33 @@ contract Safe {
     return createSafe(ownerArr, 1);
   }
 
+  function create2Safe(address [] memory safeOwners, uint256 threshold, uint256 nonce) internal returns (address){
+    bytes memory data =
+      abi.encodeWithSelector(
+        SETUP,
+        safeOwners,
+        threshold,
+        ZERO_ADDRESS,
+        "",
+        ZERO_ADDRESS,
+        ZERO_ADDRESS,
+        0,
+        ZERO_ADDRESS
+      );
+    address safe =
+      address(
+        GnosisSafeProxyFactory(gnosisProxyFactory).createProxyWithNonce(gnosisSafe, data, nonce)
+      );
+
+    require(safe != ZERO_ADDRESS, "Create a Safe failed");
+    return safe;
+  }
+
+  function create2Safe(address owner, uint256 nonce) internal returns (address) {
+    address[] memory ownerArr = new address[](1);
+    ownerArr[0] = owner;
+
+    return create2Safe(ownerArr, 1, nonce);
+  }
   uint256[50] private ____gap;
 }
