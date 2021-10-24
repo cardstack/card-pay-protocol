08a4cab76e1a2f5ac6d45a838fed9f7ee29c533a
Justin <justinthong93@gmail.com>
2021-10-24 10:33:35 +0800

    fix casing

--- contracts/commerce-protocol/Contracts.sol
+++ contracts/commerce-protocol/Contracts.sol
@@ -5,7 +5,7 @@ pragma solidity 0.6.8;
 // This contract exists solely to pull in the contracts from the CardPay-Contracts package,
 // so that they are built by hardhat and included in the artifacts directory
 
-import "CardPay-Contracts/contracts/BaseERC20.sol";
+import "CardPay-Contracts/contracts/BaseErc20.sol";
 import "CardPay-Contracts/contracts/ExchangeMock.sol";
 import "CardPay-Contracts/contracts/Inventory.sol";
 import "CardPay-Contracts/contracts/LevelRegistrar.sol";
