ceb8860909a6b305c54b5dabd680fb412fd34d0e
Alex Cardstack <93225030+alex-cardstack@users.noreply.github.com>
2021-11-24 20:47:13 +0000

    Fix solcov config

--- .solcover.js
+++ .solcover.js
@@ -1,11 +1,7 @@
 module.exports = {
   skipFiles: [
-    // This contract is only used to pull in npm solidity artifacts used for
-    // testing
-    "contracts/dev/DevDependencies.sol",
     // This contract is a mock malicious contract and the tests make sure the code is _not_ run
-    "contracts/dev/FakeRewardManager.sol",
-    "contracts/deprecated/*.sol",
+    "dev/FakeRewardManager.sol",
   ],
   providerOptions: {
     total_accounts: 1000,
