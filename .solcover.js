cbcbcff0d1a686cb81fd8e0be11646caae664564
Alex Cardstack <93225030+alex-cardstack@users.noreply.github.com>
2022-02-02 20:21:34 +0000

    Coverage skip migration contracts

--- .solcover.js
+++ .solcover.js
@@ -2,8 +2,17 @@ module.exports = {
   skipFiles: [
     // This contract is a mock malicious contract and the tests make sure the code is _not_ run
     "dev/FakeRewardManager.sol",
-    // solcover does not like the assembly syntax in here 
-    "migration/EnumerableSetUpgradeUtil.sol"
+
+    // Migration contracts not run in coverage tests
+    "migration/EnumerableSetUpgradeUtil.sol",
+    "migration/MerchantManagerUpgrader.sol",
+    "migration/PrepaidCardManagerUpgrader.sol",
+    "migration/PrepaidCardMarketUpgrader.sol",
+    "migration/RevenuePoolUpgrader.sol",
+    "migration/RewardManagerUpgrader.sol",
+    "migration/RewardPoolUpgrader.sol",
+    "migration/SPENDUpgrader.sol",
+    "migration/TokenManagerUpgrader.sol",
   ],
   providerOptions: {
     total_accounts: 1000,
