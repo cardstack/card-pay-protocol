ccf18d864f579efcb1ce5da1a64561c1cb5289c7
Alex Cardstack <93225030+alex-cardstack@users.noreply.github.com>
2022-02-02 18:35:12 +0000

    Fix coverage

--- .solcover.js
+++ .solcover.js
@@ -2,6 +2,8 @@ module.exports = {
   skipFiles: [
     // This contract is a mock malicious contract and the tests make sure the code is _not_ run
     "dev/FakeRewardManager.sol",
+    // solcover does not like the assembly syntax in here 
+    "migration/EnumerableSetUpgradeUtil.sol"
   ],
   providerOptions: {
     total_accounts: 1000,
