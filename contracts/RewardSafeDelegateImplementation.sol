d6fb3cf2d388c80ae42686c902a49ee5cd659780
Alex Cardstack <93225030+alex-cardstack@users.noreply.github.com>
2022-03-02 16:50:31 +0000

    Add a reentrancy guard to delegate safe implementation

--- contracts/RewardSafeDelegateImplementation.sol
+++ contracts/RewardSafeDelegateImplementation.sol
@@ -31,12 +31,38 @@ contract RewardSafeDelegateImplementation {
     address newOwner
   );
 
+  // Because this is a delegate implementation, the storage of this contract is the safe storage.
+  // For that reason, we have to do assembly hax to store the reentrancy flag and can't just use
+  // ReentrancyGuardUpgradeable
+
+  // bytes32(uint256(keccak256("safe.delegate.reentrancy_flag")) - 1)
+  bytes32 internal constant REENTRANCY_GUARD_SLOT =
+    0x7712fbd67a2d5bda218f5373b681e9e155932a5fa44c9a20ed14fbb50f32636f;
+
+  modifier nonReentrant() {
+    bool _entered;
+    assembly {
+      _entered := sload(REENTRANCY_GUARD_SLOT)
+    }
+
+    require(!_entered, "reentrant call");
+
+    assembly {
+      sstore(REENTRANCY_GUARD_SLOT, true)
+    }
+    _;
+
+    assembly {
+      sstore(REENTRANCY_GUARD_SLOT, false)
+    }
+  }
+
   function withdraw(
     address __trusted__managerContract,
     address __untrusted__token,
     address __untrusted__to,
     uint256 __untrusted__value
-  ) external {
+  ) nonReentrant external {
     require(
       RewardManager(__trusted__managerContract).isValidToken(
         __untrusted__token
